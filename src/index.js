c47885ae2c878f04803a6887f057a8185f425b78
Henrik Sandberg <henrik.a.sandberg@icloud.com>
2018-10-14 15:28:20 +0200

    first time i have setup of scss

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+import '../node_modules/normalize.css/normalize.css';
+import './styles/index.scss';
 
 ReactDOM.render(<IndecisionApp/>, document.getElementById("root"));
